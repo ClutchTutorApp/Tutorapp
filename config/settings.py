900c468264eba25b8981d1fca1ba59cbc76bcae2
Gutenberg Petit-vil <gutenbergpetit-vil@Gutenbergs-MacBook-Air.local>
2026-05-11 00:16:58 -0400

    Add early warning notebooks, proposal doc, update settings

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ if env_file.exists():
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY', 'wdw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
